a56e4596d8ec51a452465e85c0b2c10e6d90777c
hendryhb <hensi27@gmail.com>
2025-11-06 13:04:27 +0800

    solved: swap case

--- 3.js
+++ 3.js
@@ -1,5 +1,22 @@
 function swapLowerUpper(sentence) {
   // write your code here!
+  // console.log(sentence)
+  let result = '';
+
+  for (let i = 0; i < sentence.length; i++) {
+    // console.log(sentence[i]);
+    let perChar = sentence[i];
+    if (perChar === perChar.toUpperCase() && perChar !== perChar.toLowerCase()) {
+      result += perChar.toLowerCase();
+      // console.log(result, "<< result Lowercase");
+    } else if (perChar === perChar.toLowerCase() && perChar !== perChar.toUpperCase()) {
+      result += perChar.toUpperCase();
+      // console.log(result, "<< result Uppercase");
+    } else {
+      result += perChar;
+    }
+  }
+  return result;
 }
 
 // TEST CASES
